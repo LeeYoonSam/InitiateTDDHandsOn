80c47fb2cb0e3a13cd514329e8e7bd9fed39ddb5
YS <yoonsam86@gmail.com>
2021-03-15 23:47:50 +0900

    기존에 작성한 TEXT-REFINER 코드 테스트 우선 개발 실습 추가

--- TEXT-REFINER/index.test.js
+++ TEXT-REFINER/index.test.js
@@ -1,21 +1,22 @@
+const faker = require("faker")
 // sut - system uner test
 const sut = require("./index")
 
 // 입력되는 문자열을 다르게해서 테스트
-test('sut trasnforms "hello  world" to "hello world', () => {
-    const actual = sut("hello  world");
-    expect(actual).toBe("hello world");
-});
+// test('sut trasnforms "hello  world" to "hello world', () => {
+//     const actual = sut("hello  world");
+//     expect(actual).toBe("hello world");
+// });
 
-test('sut trasnforms "hello    world" to "hello world', () => {
-    const actual = sut("hello    world");
-    expect(actual).toBe("hello world");
-});
+// test('sut trasnforms "hello    world" to "hello world', () => {
+//     const actual = sut("hello    world");
+//     expect(actual).toBe("hello world");
+// });
 
-test('sut trasnforms "hello   world" to "hello world', () => {
-    const actual = sut("hello   world");
-    expect(actual).toBe("hello world");
-});
+// test('sut trasnforms "hello   world" to "hello world', () => {
+//     const actual = sut("hello   world");
+//     expect(actual).toBe("hello world");
+// });
 
 /**
  * 반복되는 코드를 줄이고싶다
@@ -26,12 +27,12 @@ test('sut trasnforms "hello   world" to "hello world', () => {
  * - 테스트가 실패하면 이후 테스트는 진행하지 않기 때문에 이후 테스트가 성공인지 실패인지 알수 없다.
  * - 코드량이 줄어든다고 무조건 좋은것이 아니다.
  */
-test('sut correctly works', () => {
-    for(const source of ['hello  world', 'hello   world', 'hello    world']) {
-        const actual = sut(source);
-        expect(actual).toBe("hello world");
-    }
-});
+// test('sut correctly works', () => {
+//     for(const source of ['hello  world', 'hello   world', 'hello    world']) {
+//         const actual = sut(source);
+//         expect(actual).toBe("hello world");
+//     }
+// });
 
 /**
  * Jest - Parameterized Test 사용
@@ -43,11 +44,43 @@ test('sut correctly works', () => {
  * 피드백 품질 향상
  */
 test.each`
-    source              | expected
-    ${"hello  world"}   | ${"hello world"}
-    ${"hello   world"}  | ${"hello world"}
-    ${"hello    world"} | ${"hello world"}
+    source                  | expected
+    ${"hello  world"}       | ${"hello world"}
+    ${"hello   world"}      | ${"hello world"}
+    ${"hello    world"}     | ${"hello world"}
+    ${"hello     world"}    | ${"hello world"}
+    ${"hello      world"}   | ${"hello world"}
+    ${"hello       world"}  | ${"hello world"}
 `('sut transforms "$source" to "$expected"', ({ source, expected }) => {
     const actual = sut(source);
     expect(actual).toBe(expected);
+});
+
+test.each`
+    source              | expected
+    ${"hello\t world"}  | ${"hello world"}
+    ${"hello \tworld"}  | ${"hello world"}
+`('sut transforms "$source" that contains tab character to "$expected"', ({ source, expected }) => {
+    const actual = sut(source);
+    expect(actual).toBe(expected);
+});
+
+test.each`
+    source | bannedWords | expected
+    ${"hello mockist"} | ${["mockist", "purist"]} | ${"hello *******"}
+    ${"hello purist"} | ${["mockist", "purist"]} | ${"hello ******"}
+`('sut transforms "$source" to $"expected"', ({ source, bannedWords, expected}) => {
+    const actual = sut(source, { bannedWords });
+    expect(actual).toBe(expected)
+});
+
+describe('given banned word', () => {
+    const bannedWord = faker.lorem.word();
+    const source = "hello " + bannedWord;
+    const expected = "hello " + "*".repeat(bannedWord.length);
+
+    test(`${bannedWord} when invoke sut hent it returns ${expected}`, () => {
+        const actual = sut(source, { bannedWords: [bannedWord] });
+        expect(actual).toBe(expected);
+    });
 });
